7fc5b64e0a1e2acc799fda636f0cca68e9f8d48a
Ethan <ethan.steip@gmail.com>
2022-12-15 13:11:49 -0500

    updating queries to use parameterized queries to protect against sql injections

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,4 @@
 const { Pool } = require('pg');
-
 const pool = new Pool({
   user: 'labber',
   password: 'labber',
@@ -7,17 +6,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-//const args = process.argv;
+const queryString = `
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name = $1
+  ORDER BY teacher;
+`;
+
+const cohortName = [`${process.argv[2] || 'JUL02'}`];
+
+pool.query(queryString, cohortName)
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
